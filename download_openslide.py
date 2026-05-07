086c2c9894d1b7fb39e3209e5108b6293a4e183b
David Völlm <david@voellm-online.de>
2026-05-07 15:42:20 +0200

    Fix Allow Overwrite of existing openslide installation

--- download_openslide.py
+++ download_openslide.py
@@ -9,52 +9,63 @@ import requests
 from bs4 import BeautifulSoup
 import zipfile
 
-openslide_url = "https://openslide.org/download/"
-
-response = requests.get(openslide_url)
-
-# Determine OS to choose openslide version
-operatingSystem:str = ""
-operatingSystemInformation = platform.uname()
-if operatingSystemInformation.system == "Windows":
-    operatingSystem = "windows-x64" if "64" in operatingSystemInformation.machine else "win32"
-elif operatingSystemInformation.system == "Linux":
-    if "x86_64" in operatingSystemInformation.machine: 
-        operatingSystem = "linux-x86_64"
-    elif "aarch64" in operatingSystemInformation.machine:
-        operatingSystem = "linux-aarch64"
-elif operatingSystemInformation.system == "Darwin":
-    operatingSystem = "macos"
-if operatingSystem == "":
-    raise Exception("Can't determin Operating System, therefore no openslide installation is possible")
+# Check for Openslide Installation
+continueInstalling = True
+if(os.path.isdir("openslide")):
+    userAnswer = input("There is already an Openslide installation. Do you want to continue and overwrite it? [Y/N]")
+    if not (userAnswer.lower in ["y","yes", "j", "ja"]):
+        continueInstalling = False
 
 
-if response.status_code == 200:
-    soup = BeautifulSoup(response.text, 'html.parser')
 
-    download_link = None
-    for link in soup.find_all('a'):
-        if link.get('href') and operatingSystem in link.get('href'):
-            download_link = link.get('href')
-            break
+if continueInstalling:
+    # Determine OS to choose openslide version
+    operatingSystem:str = ""
+    operatingSystemInformation = platform.uname()
+    if operatingSystemInformation.system == "Windows":
+        operatingSystem = "windows-x64" if "64" in operatingSystemInformation.machine else "win32"
+    elif operatingSystemInformation.system == "Linux":
+        if "x86_64" in operatingSystemInformation.machine: 
+            operatingSystem = "linux-x86_64"
+        elif "aarch64" in operatingSystemInformation.machine:
+            operatingSystem = "linux-aarch64"
+    elif operatingSystemInformation.system == "Darwin":
+        operatingSystem = "macos"
+    if operatingSystem == "":
+        raise Exception("Can't determin Operating System, therefore no openslide installation is possible")
 
-    if download_link:
-        filename = os.path.basename(download_link)
+    # Download Openslide
+    openslide_url = "https://openslide.org/download/"
 
-        response = requests.get(download_link)
-        if response.status_code == 200:
-            with open(filename, 'wb') as file:
-                file.write(response.content)
+    response = requests.get(openslide_url)
 
-            with zipfile.ZipFile(filename) as zip_ref:
-                zip_ref.extractall()
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
 
-            os.remove(filename)
-            os.rename(filename[:-4], "openslide")
-            print(f"Openslide has been successfully downloaded.")
+        download_link = None
+        for link in soup.find_all('a'):
+            if link.get('href') and operatingSystem in link.get('href'):
+                download_link = link.get('href')
+                break
+
+        if download_link:
+            filename = os.path.basename(download_link)
+
+            response = requests.get(download_link)
+            if response.status_code == 200:
+                with open(filename, 'wb') as file:
+                    file.write(response.content)
+
+                with zipfile.ZipFile(filename) as zip_ref:
+                    zip_ref.extractall()
+
+                os.remove(filename)
+                if(os.path.isdir("openslide")): os.remove("openslide") # Overwrite existing installation
+                os.rename(filename[:-4], "openslide")
+                print(f"Openslide has been successfully downloaded.")
+            else:
+                print("Failed to download OpenSlide library.")
         else:
-            print("Failed to download OpenSlide library.")
+            print("Download link not found on the page.")
     else:
-        print("Download link not found on the page.")
-else:
-    print("Failed to access the OpenSlide download page.")
+        print("Failed to access the OpenSlide download page.")
