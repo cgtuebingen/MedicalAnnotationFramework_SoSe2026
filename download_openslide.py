78e3f6462467e659b5eef6fb209a83f8466415b9
David Völlm <david@voellm-online.de>
2026-05-07 15:25:29 +0200

    Feat download openslide for macOS and Linux

--- download_openslide.py
+++ download_openslide.py
@@ -4,6 +4,7 @@ Only works for windows currently
 """
 #TODO: Makes this work for other operating systems
 import os
+import platform
 import requests
 from bs4 import BeautifulSoup
 import zipfile
@@ -12,12 +13,28 @@ openslide_url = "https://openslide.org/download/"
 
 response = requests.get(openslide_url)
 
+# Determine OS to choose openslide version
+operatingSystem:str = ""
+operatingSystemInformation = platform.uname()
+if operatingSystemInformation.system == "Windows":
+    operatingSystem = "windows-x64" if "64" in operatingSystemInformation.machine else "win32"
+elif operatingSystemInformation.system == "Linux":
+    if "x86_64" in operatingSystemInformation.machine: 
+        operatingSystem = "linux-x86_64"
+    elif "aarch64" in operatingSystemInformation.machine:
+        operatingSystem = "linux-aarch64"
+elif operatingSystemInformation.system == "Darwin":
+    operatingSystem = "macos"
+if operatingSystem == "":
+    raise Exception("Can't determin Operating System, therefore no openslide installation is possible")
+
+
 if response.status_code == 200:
     soup = BeautifulSoup(response.text, 'html.parser')
 
     download_link = None
     for link in soup.find_all('a'):
-        if link.get('href') and "windows-x64" in link.get('href'):
+        if link.get('href') and operatingSystem in link.get('href'):
             download_link = link.get('href')
             break
 
