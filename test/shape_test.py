9f99169e58de0aa8a05ab39190d4a073d7f24bb6
Toffle666 <seidenschnabel307@gmail.com>
2026-06-26 11:41:43 +0200

    added test

--- test/shape_test.py
+++ test/shape_test.py
@@ -4,7 +4,7 @@ from PySide6.QtWidgets import QApplication, QGraphicsScene, QGraphicsSceneMouseE
 from PySide6.QtCore import QSize, QPointF, QEvent
 from PySide6.QtGui import Qt
 from taplt.ui.shape import Shape
-
+from taplt.ui.annotation_group import AnnotationGroup
 
 app = QApplication.instance() or QApplication(sys.argv)
 
@@ -45,3 +45,27 @@ def test_circle_not_closed_after_1_point():
     scene.addItem(shape)
     shape.mousePressEvent(make_mouse_event(QPointF(100, 200)))
     assert shape.is_closed_path == False
+
+def test_forward_first_click():
+    scene = QGraphicsScene()
+    scene.sceneRect(0,0,200,200)
+    annotation_group = AnnotationGroup()
+    scene.addItem(annotation_group)
+
+    event = QGraphicsSceneMouseEvent(QEvent.GraphicsSceneMousePress)
+    event.setButton(Qt.MouseButton.LeftButton)
+    event.setPos(QPointF(100,100))
+    event.setScenePos(QPointF(100,100))
+
+    annotation_group.set_type(Shape.ShapeType.POINT)
+    annotation_group.create_shape(event)
+
+    assert annotation_group.temp_shape is not None
+    assert len(annotation_group.temp_shape.vertices.vertices) == 1
+
+    annotation_group.set_type(Shape.ShapeType.POLYGON)
+    annotation_group.create_shape(event)
+
+    assert annotation_group.temp_shape is not None
+    assert len(annotation_group.temp_shape.vertices.vertices) == 1
+
