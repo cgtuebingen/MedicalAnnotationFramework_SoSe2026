c68d7c588b7c34ae686074c85e83e35c845ac249
clarawirmel <clawirmel@gmail.com>
2026-07-09 17:14:22 +0200

    add icon to welcome screen

--- taplt/ui/welcome_screen.py
+++ taplt/ui/welcome_screen.py
@@ -1,7 +1,7 @@
 import os
 
-from PySide6.QtWidgets import QWidget, QHBoxLayout, QLabel
-from PySide6.QtGui import QFont
+from PySide6.QtWidgets import QWidget, QHBoxLayout, QLabel, QSizePolicy
+from PySide6.QtGui import QFont, QPixmap
 from PySide6.QtCore import Qt
 from taplt import source_directory
 from taplt.utils.stylesheets import BASE_FONT_SIZE
@@ -10,18 +10,52 @@ from taplt.utils.stylesheets import BASE_FONT_SIZE
 class WelcomeScreen(QWidget):
 
     def __init__(self):
-        super(WelcomeScreen, self).__init__()
+        super().__init__()
 
-        self.label = QLabel()
-        self.label.setText("Welcome to the \n \n"
-                           "All-Purpose Labeling Tool \n \n \n"
-                           "Create or open a project to get started")
-        self.label.setFont(QFont("Helvetica", BASE_FONT_SIZE + 2, QFont.Weight.Bold))
-        self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        icon = os.path.join(source_directory, 'icons', 'welcome.jpg').replace("\\", "/")
-        self.label.setStyleSheet(f"background-image: url('{icon}');"
-                                 "background-repeat: no-repeat;"
-                                 "background-position: center;")
+        icon_path = os.path.join(source_directory, 'icons', 'logo.png').replace("\\", "/")
+        pixmap = QPixmap(icon_path)
 
-        self.setLayout(QHBoxLayout())
-        self.layout().addWidget(self.label)
+        # Widget für Logo
+        self.icon_label = QLabel()
+        self.icon_label.setPixmap(pixmap)
+        self.icon_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.icon_label.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+
+        # Name
+        self.title_label = QLabel("All-Purpose Labeling Tool")
+        self.title_label.setFont(QFont("Oswald", BASE_FONT_SIZE + 12, QFont.Weight.Bold))
+        self.title_label.setAlignment(Qt.AlignLeft)
+
+        # Untertitel
+        self.subtitle_label = QLabel("Create or open a project to get started")
+        self.subtitle_label.setFont(QFont("Oswald", BASE_FONT_SIZE + 2, QFont.Weight.Bold))
+        self.subtitle_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        
+        # Layout
+        # Logo + Titel
+        top_row = QHBoxLayout()
+        top_row.addWidget(self.icon_label)
+        top_row.addWidget(self.title_label)
+        top_row.setSpacing(20)
+        top_row.setContentsMargins(0, 0, 0, 0)
+
+        main_layout = QHBoxLayout()
+        main_layout.addLayout(top_row)
+        main_layout.addWidget(self.subtitle_label)
+        main_layout.setSpacing(15)
+        main_layout.setContentsMargins(20, 20, 20, 20)
+        main_layout.setAlignment(Qt.AlignCenter)
+
+        self.setLayout(main_layout)
+
+        def resizeEvent(self, event):
+            """Logo dynamisch skalieren."""
+            w = self.width()
+            target_width = max(120, min(0.22 * w, 300))
+
+            pixmap = QPixmap(os.path.join(source_directory, 'icons', 'logo.png'))
+            scaled = pixmap.scaledToWidth(int(target_width), Qt.SmoothTransformation)
+            self.icon_label.setPixmap(scaled)
+
+            super().resizeEvent(event)
+
