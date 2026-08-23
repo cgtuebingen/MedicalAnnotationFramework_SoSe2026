7dd563a0877bca432b3305ad47d2866709c8c34a
clarawirmel <clawirmel@gmail.com>
2026-08-23 12:14:28 +0200

    container for right top elements

--- taplt/ui/main_window.py
+++ taplt/ui/main_window.py
@@ -136,6 +136,11 @@ class LabelingMainWindow(QMainWindow):
 
         self.menubar = MenuBar(self)
         self.setMenuBar(self.menubar)
+        self.corner_container = QWidget()
+        self.corner_container.setLayout(QHBoxLayout())
+        self.corner_container.layout().setContentsMargins(0, 0, 8, 0)
+        self.corner_container.layout().setSpacing(4)
+        self.corner_container.layout().addWidget(self.menubar.nav_widget)
         self.right_panel_toggle = QToolButton(self)
         self.right_panel_toggle.setText("☰")
         self.right_panel_toggle.setCheckable(True)
@@ -143,7 +148,8 @@ class LabelingMainWindow(QMainWindow):
         self.right_panel_toggle.setToolTip("Show/hide side panel")
         self.right_panel_toggle.setStyleSheet("QToolButton { font-size: 16px; padding: 2px 8px; }")
         self.right_panel_toggle.clicked.connect(self.toggle_right_panel)
-        self.menubar.setCornerWidget(self.right_panel_toggle, Qt.TopRightCorner)
+        self.corner_container.layout().addWidget(self.right_panel_toggle)
+        self.menubar.setCornerWidget(self.corner_container, Qt.TopRightCorner)
         self.menubar.setVisible(True)
 
         self.statusbar = QStatusBar()
